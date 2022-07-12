a8e9714bb583a31ff5f2f3a222eefa537770b57f
karoon_01 <xblazeradd@gmail.com>
2022-07-12 14:02:07 +0300

    hometask 3

---
+++ src/main/java/com/epam/spring/homework3/service/ActivityService.java
@@ -0,0 +1,21 @@
+package com.epam.spring.homework3.service;
+
+import com.epam.spring.homework3.model.DTO.ActivityDTO;
+import com.epam.spring.homework3.model.DTO.UserDTO;
+
+import java.util.List;
+
+public interface ActivityService {
+    ActivityDTO createActivity(ActivityDTO activityDTO);
+
+    ActivityDTO getActivity(Long id);
+
+    ActivityDTO updateActivity(Long id, ActivityDTO activityDTO);
+
+    void deleteActivity(Long id);
+
+    List<ActivityDTO> getAllActivities();
+
+    List<UserDTO> getAllActivityUsers(Long activityId);
+
+}
